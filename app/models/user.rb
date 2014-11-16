b060d8df64423898cea8fc05c80555a234df6b89
Jared Smith <jared.rit@gmail.com>
2014-11-15 21:50:12 -0500

    Temporarily disable :confirmable for users while we wait for email integration for local development

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
