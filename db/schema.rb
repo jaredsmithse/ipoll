9c7ac127e232964080c3d8c8d2f05b2b9f300a2e
Jared Smith <jared.rit@gmail.com>
2014-11-16 08:30:04 -0500

    Add attributes to polls that must've been overlooked :cry:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116120051) do
+ActiveRecord::Schema.define(version: 20141116130752) do
 
   create_table "answers", force: true do |t|
     t.string  "description"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20141116120051) do
   end
 
   create_table "polls", force: true do |t|
+    t.integer "question_id"
+    t.integer "user_id"
+    t.integer "answer_id"
   end
 
   create_table "questions", force: true do |t|
