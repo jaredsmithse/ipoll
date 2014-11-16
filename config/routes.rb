ab5aa4541a88f4b7c1f03750e80465dd99eb339e
Jared Smith <jared.rit@gmail.com>
2014-11-16 01:52:43 -0500

    Overwrite my controllers for devise so i can work in the role attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "my_devise/registrations" }
   resources :users
   resources :rooms
 end
